fc7bbaf432b58f789053e79b8771a7df64af65ee
Christoph Buelter <christoph.buelter@myshopradio.de>
2022-12-07 14:55:14 +0100

    Collect opened filenames and add as sorted text into new buffer

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,27 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-list-opened-files-as-text" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-list-opened-files-as-text.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-list-opened-files-as-text!');
+	let disposable = vscode.commands.registerCommand('vscode-list-opened-files-as-text.listOpenedFilesAsText', () => {
+
+		const fileNames = [];
+		for (const textDocument of vscode.workspace.textDocuments) {
+			fileNames.push(textDocument.fileName);
+		}
+		fileNames.sort();
+		const text = fileNames.join('\n');
+
+		vscode.commands.executeCommand('workbench.action.files.newUntitledFile')
+			.then(() => {
+				const editor = vscode.window.activeTextEditor;
+				if (editor) {
+					editor.edit(editBuilder => {
+						editBuilder.insert(editor.selection.active, text);
+					});
+				}
+			});
 	});
 
 	context.subscriptions.push(disposable);
